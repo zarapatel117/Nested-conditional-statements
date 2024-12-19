0a31aba0a8a55835e3429fa79a39b1250fbd7dfd
zarapatel117 <zarapatel2510@gmail.com~>
2024-12-19 20:57:02 +0530

    choose your ride

---
+++ Customise your ride.py
@@ -0,0 +1,26 @@
+print ("option 1 = car ride ")
+print ("option 2 = bike ride ")
+q= int(input("choose a option 1 or 2 "))
+if q==1:
+    print ("option 1 = maruti suzuki ")
+    print ("option 2 = mercedes ")
+    w=int (input("choose one option 1 or 2 "))
+    if w==1:
+        print(" you chose maruti suzuki, enjoy your ride ")
+    else :
+        print ("you chose mercedes, enjoy your ride ")      
+elif q==2:
+    print ("option 1 = scooty ")
+    print ("option 2 = activa ")
+    e=int (input("choose one option 1 or 2 "))
+    if e==1:
+        print(" you chose scooty, enjoy your ride ")
+    else :
+        print ("you chose activa , enjoy your ride ")
+else :
+    print ("invalid option " )        
+
+
+
+    
+
