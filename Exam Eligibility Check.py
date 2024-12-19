4382f69036150925c073e919cf6fbbcff50d5290
zarapatel117 <zarapatel2510@gmail.com~>
2024-12-19 20:26:26 +0530

    Exam Eligibility Check

---
+++ Exam Eligibility Check.py
@@ -0,0 +1,8 @@
+q=input("Do you have a medical cause or not y for yes, n for no ")
+w= int(input("what is your attendence n"))
+if q=="y":
+    print ("you are allowed in the exam ")
+elif w>=75 :
+    print ("you are allowed in the exam")
+else :
+    print (" you cannot give the exam")
